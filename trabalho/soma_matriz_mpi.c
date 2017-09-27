1547a8d720a8ca523cd6f689445497e6d659366f
jgfenix <jgfenix@gmail.com>
2017-09-27 11:03:24 -0300

    mpi OK

--- trabalho/soma_matriz_mpi.c
+++ trabalho/soma_matriz_mpi.c
@@ -1,90 +1,105 @@
-//escreva um programa que calcula a soma de 
+// escreva um programa que calcula a soma de 
 // todos os elementos de uma matriz A(n x n). 
 // Considere que o processo 0 recebe como entrada da linha de comando
 // o valor n, gera os elementos da matriz de modo randomico e distribui
 // a computacao da soma pelos processos
 
 // Na saida, o processo 0 deve imprimir o valor da soma de todos os elementos
-//28000
+
 #include <stdio.h>
 #include <math.h> //random
 #include <stdlib.h> //atoi, exit
 #include <sys/time.h> //cronometro
+#include <mpi.h>
 
-#define TIPO double
+int main(int argc, char const *argv[]) {
+	int tag=0, i=0, j=0, num_processos, ID_processo;
+	double sum=0, local_sum=0;
 
-int main(int argc, char const *argv[])
-{
-	//verificacao de passagem de parametros	
+	MPI_Status status;	
+
+	MPI_Init(NULL, NULL);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &num_processos);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &ID_processo);
+
 	if (argv[1] == NULL || atoi(argv[1]) < 1) {
-		printf("ERRO, insira o valor de 'n':\n\nEx: mpirun -np [numero de processos] %s [tamanho do vetor] \n", argv[0]);	
-		// printf("ERRO, insira o valor de 'n': \n");
+		printf("ERRO, insira o valor de 'n': \n");
 		exit(1);
 	}
-
+	
+	//tamanho da matriz a ser gerada
 	int n = atoi(argv[1]);
+	
+	//matriz global
+	double matriz[n][n];
 
-	FILE *in;
-	extern FILE *popen();
-	char buff[512];
+	// preenchimento da matriz
+	if (ID_processo == 0) {
+		for (i=0; i<n; i++) {
+			for (j = 0; j < n; j++)	{
+				// matriz[i][j] = random();
+				matriz[i][j] = 1.0;  
+			}
+		}
 
-	if(!(in = popen("free -m | awk 'NR==2{ print $4 }'", "r"))){
-		exit(1);
+		//envio de dados
+		for (i = 1; i < num_processos; ++i) {
+			MPI_Send(&matriz, n*n, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+		}
 	}
-	while(fgets(buff, sizeof(buff), in)!=NULL){
-		// printf("memoria livre =%d", atoi(buff));
+	else {
+		MPI_Recv(&matriz, n*n, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
 	}
-	pclose(in);
-	
 
-	printf("\n\nRAM livre:\t\t%d Mb\n", atoi(buff));
-	// int n = 1000000;
-	if ( ((int)(sizeof(TIPO) * n *n / 1048576)) > atoi(buff))
-	{	
-		printf("precisa de %f.0 Mb de RAM\n", (TIPO)(sizeof(TIPO) * n *n / 1048576));
-		printf("Nao sera possivel continuar o programa, SAINDO...\n\n");
-		exit(1);
-	}
-	printf("\nContinuando...\nAlocando %f.0 Mb RAM ...\n\n", (TIPO)sizeof(TIPO) * n *n / 1048576);
+	MPI_Barrier(MPI_COMM_WORLD);
 
-	double sum = 0;
-	int i = 0, j=0;
-	
-	TIPO **matriz ;
-	matriz = calloc(n, sizeof(TIPO));
-	for (i = 0; i < n; i++)
-      matriz[i] = calloc(n, sizeof(TIPO));
-	
-	//inicio da contagem de tempo
+	//processamento de dados
 
-	//vai ser usado no random do preenchimento
-	// srandom(tm.tv_sec + tm.tv_usec * 1000000ul);
-	
-	for (i=0; i<n; i++) {
-		for (j = 0; j < n; j++)	{
-			// matriz[i][j] = random();
-			matriz[i][j] = 1.0;  
+	//caso 0: n == word_size
+	if (n == num_processos) {
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; ++j) {
+				if (ID_processo == i) {
+					local_sum+= matriz[i][j];
+				}
+			}
 		}
 	}
 
-	//inicio da contagem de tempo
-	struct timeval  tv1, tv2;
-	gettimeofday(&tv1, NULL);
-
-	for (i=0; i<n; i++) {
-		for (j = 0; j < n; j++)	{
-			sum += matriz[i][j];
+	//caso 1 e caso 2:
+		//1:	n > word_size : algum processo vai ficar sobrecarregado
+		//2:	n < word_size : algum processo vai ficar ocioso
+	// o ideal seria distribuir proporcionalmente para cada processo,
+	// mas isso é uma refinação desnecessária quadno o objetivo é entender
+	// o uso da biblioteca
+	else {
+		int counter = 0;
+		for (i = 0; i < n; i++) {
+			// printf("counter==%d\n",counter);
+			for (j = 0; j < n; ++j) {
+				if (ID_processo == counter) {
+					local_sum+= matriz[i][j];
+				}
+			}
+			if (counter < ID_processo)
+				counter++;
+			else
+				counter = num_processos-1;
 		}
 	}
 
+	MPI_Barrier(MPI_COMM_WORLD);
 
-	gettimeofday(&tv2, NULL);
-	printf ("Total time = %f seconds\n",
-         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-         (double) (tv2.tv_sec - tv1.tv_sec));
+	printf("local_sum=%f, ID_processo=%d\n", local_sum,ID_processo);
 
-	free(matriz);
-
-	printf("sum = %f \n", sum );
+	//coleta de dados
+	MPI_Reduce(&local_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	if (ID_processo == 0) {
+		printf("\n\nsum=%f \n", sum);
+	}
+	
+	MPI_Finalize();
 	return 0;
 }
