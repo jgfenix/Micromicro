98e23ce20fd69655ae6cbb23c7cedea0d6555982
jgfenix <jgfenix@gmail.com>
2017-12-10 11:19:02 -0200

    alteracoes propostas pela prof

--- trabalho/soma_matriz_pthread.c
+++ trabalho/soma_matriz_pthread.c
@@ -3,71 +3,66 @@
 #include <sys/time.h> //cronometro
 #include <pthread.h>
 
-
 #ifndef PROCESSOS_E_THREADS
 #define PROCESSOS_E_THREADS 2
 #endif
 
-struct matriz_e_tamanho {
-	double** m; //ponteiro pra matriz
-	long n; //tamanho da matriz quadrada
-	double sum;
-	pthread_mutex_t mutex;
-	pthread_t thread[PROCESSOS_E_THREADS];
-	pthread_cond_t cond;
-};
-
 struct timeval  tv1, tv2;
+long n;
+double **matriz;
+pthread_mutex_t mutex;
+double sum = 0.0;
 
-// TODO(se der tempo): Thread parent so esta gerando e distribuindo a matriz, nao esta processando ela
-
-//controle do numero de threads childs
-long tID = 0; //thread 0 é a thread que gera a matriz
-int controle_linhas=0;
-
-void *ProcessaMatriz(void *m_n) {
-	//struct passada como parametro pra thread executar
-	struct matriz_e_tamanho *mat_n = (struct matriz_e_tamanho *)m_n;
-
-	pthread_mutex_lock(&mat_n->mutex);//bloqueia a regiao critica
-		long local_tID = tID;
-		tID++;
-		while(tID< PROCESSOS_E_THREADS) //bloqueia a thread ate que todos tenham um ID
-			pthread_cond_wait( &mat_n->cond, &mat_n->mutex );
-
-		pthread_cond_signal(&mat_n->cond);//sinaliza para as threads continuarem pois todas pegaram um ID
-	pthread_mutex_unlock(&mat_n->mutex);//desbloqueia a regiao critica
+void *ProcessaMatriz(void *t_ID) {
+	int * t = (int *) t_ID;
+	int t_child = * t;
+	
 
 	int i=0, //linhas
-		j=0, //colunas
-		tamanho_sub_matriz = mat_n->n / PROCESSOS_E_THREADS;
+		j=0; //colunas
 	
 	double local_sum=0;
-	
+	if (t_child == 0) {
+		//inicio da contagem de tempo
+		gettimeofday(&tv1, NULL);
+	}
 	//processamento de dados
-	local_sum=0;
-	for (i = tamanho_sub_matriz*local_tID; i < ((local_tID+1)*tamanho_sub_matriz) ; ++i) {
-		for (j = 0; j < mat_n->n; ++j)
+	for (i = (n/PROCESSOS_E_THREADS)*t_child; i < ((t_child+1)*(n/PROCESSOS_E_THREADS)) ; ++i) {
+		for (j = 0; j < n; ++j)
 		{
-			local_sum += mat_n->m[i][j];
-			// printf("thread %ld | m[%d][%d]=%.0f\n",local_tID,i,j, mat_n->m[i][j] );
+			local_sum += matriz[i][j];
+			// printf("thread %ld | m[%d][%d]=%.0f\n",t_child,i,j, m[i][j] );
 		}
 	}
 	//mutex para nao ter mais de uma thread acessando a mesma variavel comum a todas
-	pthread_mutex_lock(&mat_n->mutex);//bloqueia a regiao critica
-		mat_n->sum+= local_sum;
-	pthread_mutex_unlock(&mat_n->mutex);//desbloqueia a regiao critica
+	pthread_mutex_lock(&mutex);//bloqueia a regiao critica
+		sum+= local_sum;
+	pthread_mutex_unlock(&mutex);//desbloqueia a regiao critica
 
+	if (t_child == 0) {
+		gettimeofday(&tv2, NULL);
+		printf ("Total time = %f seconds\n",
+         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+         (double) (tv2.tv_sec - tv1.tv_sec));
+
+	}
 	return NULL;
 }
 
-void *CriaMatriz(void *tamanhoMatriz) {
-	long n ;
+
+int main (int argc, char *argv[]) {
+	//verificacao de passagem de parametros	
+	if (argv[1] == NULL || atoi(argv[1]) < 1) {
+		printf("ERRO, insira o valor de 'n': \n");
+		exit(1);
+	}
+
+	n = (long)atoi(argv[1]);
+
 	double confere=0; //variavel so pra conferir se as threads estao somando corretamente
-	n = (long)tamanhoMatriz;
 	int i, j;
  
-	double **matriz = (double**)malloc(n * sizeof(double*)); //Aloca um Vetor de Ponteiros
+	matriz = (double**)malloc(n * sizeof(double*)); //Aloca um Vetor de Ponteiros
 	//vai ser usado no random do preenchimento
 	struct timeval tm;
 	gettimeofday(&tm, NULL);
@@ -75,70 +70,33 @@ void *CriaMatriz(void *tamanhoMatriz) {
 	for (i = 0; i < n; i++){ //Percorre as linhas do Vetor de Ponteiros
 		matriz[i] = (double*) malloc(n * sizeof(double)); //Aloca um Vetor de Inteiros para cada posição do Vetor de Ponteiros.
 		for (j = 0; j < n; j++){ //Percorre o Vetor de Inteiros atual.
-			confere+= matriz[i][j] = 1.0;//rand() % 9;
+			confere+= matriz[i][j] = rand() % 9;
 		}
 	}
-	//printf("CONFERE==%.0f\n", confere );
-	//struct para passar a matriz e o tamanho dela
-	struct matriz_e_tamanho* mat_n = malloc( sizeof( struct matriz_e_tamanho ));
-	mat_n->m = matriz;
-	mat_n->n = n;
-	mat_n->sum = 0;
+	printf("CONFERE==%.0f\n", confere );
 	
-	//inicia o mutex
-	pthread_mutex_init(&mat_n->mutex, NULL);
-	//inicia a condicao de espera
-	pthread_cond_init(&mat_n->cond, NULL);
-
-	//criacao de novas threads
-	int sub_threads = PROCESSOS_E_THREADS;
-
+	pthread_t thread[PROCESSOS_E_THREADS];
 	void *status;
+	//inicia o mutex
+	pthread_mutex_init(&mutex, NULL);
 	
-	int t_child =0;
-	//inicio da contagem de tempo
-	gettimeofday(&tv1, NULL);
+	int t_child[PROCESSOS_E_THREADS];
+	
+	// int i = 0;
 
-
-	for(t_child =0; t_child < sub_threads; t_child++) {
-		pthread_create(&(mat_n->thread[t_child]), NULL, ProcessaMatriz, mat_n); //enviar a matriz
+	for(i =0; i < PROCESSOS_E_THREADS; i++) {
+		t_child[i] = i;
+		pthread_create(&(thread[i]), NULL, ProcessaMatriz, &(t_child[i])); //enviar a matriz
 	}
 	
 	//espera todas as threads acabarem
-	for(t_child = 0; t_child < sub_threads; t_child++) {
-		pthread_join(mat_n->thread[t_child], &status);
+	for(i = 0; i < PROCESSOS_E_THREADS; i++) {
+		pthread_join(thread[i], &status);
 	}
 
-	gettimeofday(&tv2, NULL);
-	printf ("Total time = %f seconds\n",
-         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-         (double) (tv2.tv_sec - tv1.tv_sec));
-
-	printf("sum==%f\n",mat_n->sum );
+	printf("sum==%f\n",sum );
 	free(matriz);
 	pthread_exit(NULL);
-	return NULL;
-}
 
-
-int main (int argc, char *argv[])
-{
-	//verificacao de passagem de parametros	
-	if (argv[1] == NULL || atoi(argv[1]) < 1) {
-		printf("ERRO, insira o valor de 'n': \n");
-		exit(1);
-	}
-
-	long n = (long)atoi(argv[1]);
-
-	pthread_t thread0;
-	void *status;
-	
-	//thread 0 que gera a matriz
-
-	pthread_create(&thread0, NULL, CriaMatriz, (void *)n);
-
-	pthread_join(thread0, &status);
-
-	pthread_exit(NULL);
+	return 0;
 }
